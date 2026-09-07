5d3fc9574f577528687dc001fcfafaa2f8558f68
Asvin Goel <github@telematique.eu>
2026-09-07 10:33:16 +0200

    Add project from KLU-BADS/ProjectTemplate.jl

---
+++ src/Project3.jl
@@ -0,0 +1,14 @@
+"""
+    Project3
+
+A minimal Julia package to start a project from.
+"""
+module Project3
+
+# Files to be included
+include("hello.jl")
+
+# Functions to be exported
+export hello
+
+end # module Project3
